e99ac6b2d4645b226d2f66675031936a4c6cb60f
Italo Tobler <xitsilvax@hotmail.com>
2018-02-13 11:12:22 -0300

    Script is working as intended, just need to clear text of html text to make it look pretty and format the discord embed for each new post

--- designNotes.js
+++ designNotes.js
@@ -2,25 +2,118 @@
 
 var prefix = "www.dragonfable.com"
 
-function parseDNs(string, object){
-	var array = [];
-	// Parse the string
-	array[0] = "1";
-	array[1] = "2";
-	array[2] = "3";
-	array[4] = string;
-	array[5] = "5";
-	// Save info to object
-	object.author = array[0];
-	object.authorImage = array[1];
-	object.link = array[2];
-	object.title = array[3];
-	object.text = array[4];
-	object.imageLinks = array.slice(5);
+function cleanHTMLCode(string){
+	//TO DO
+}
+
+function parseDNs(array, string){
+	var i, j, k;
+	// Get link to author's icon
+	i = string.indexOf("img src=");
+	j = string.indexOf("\"", i);
+	k = string.indexOf("\"", j+1);
+	array[1] = prefix + string.substring((j+1), k);
+	// Get author name
+	i = string.indexOf("a href=\"/gamedesignnotes/tag/", 3);
+	j = string.indexOf(">", i);
+	k = string.indexOf("<", j);
+	array[0] = string.substring((j+1), k);
+	// Get date of post
+	i = string.indexOf("class=\"dateStyle\"", k);
+	j = string.indexOf(">", i);
+	k = string.indexOf("<", j);
+	array[2] = string.substring((j+1), k);
+	// Get link to the DNs
+	i = string.indexOf("a href=\"/gamedesignnotes/", k);
+	j = string.indexOf("\"", i);
+	k = string.indexOf("\"", j+1);
+	array[3] = prefix + string.substring((j+1), k);
+	// Get DNs title
+	j = string.indexOf(">", k);
+	k = string.indexOf("</a>", j);
+	array[4] = string.substring((j+1), k);
+	// Get text string
+	i = string.indexOf("<p>", k);
+	j = string.indexOf(">", i);
+	k = string.indexOf("<font color", j);
+	array[5] = string.substring((j+1), k);
+	cleanHTMLCode(array[5]);
+	// j indicates beginning of DNs text, lets have k store it instead
+	k = j;
+	// Stores all the image links inside the DN text inside the array
+	i = string.indexOf("<img src", k);
+	while(i > -1){
+		j = string.indexOf("\"", i);
+		k = string.indexOf("\"", j+1);
+		array.push(string.substring((j+1), k));
+		i = string.indexOf("<img src", k);
+	}
 }
 
 module.exports = class DesignNote{
 	constructor(string){
-		parseDNs(string, this);
+		if(string == "" || string == null){
+			// Empty constructor
+			this.author = "author";
+			this.authorImage = "authorImage";
+			this.date = "date";
+			this.link = "link";
+			this.title = "title";
+			this.text = "text";
+			this.imageLinks = ["image", "links"];
+		}else{
+			// Parsing constructor
+			var array = [];
+			// Parse the string for all information
+			parseDNs(array, string);
+			// Save info to object
+			this.author = array[0];
+			this.authorImage = array[1];
+			this.date = array[2];
+			this.link = array[3];
+			this.title = array[4];
+			this.text = array[5];
+			this.imageLinks = array.slice(6);
+		}
+	}
+
+	// Copy contents of an object (not necessarily of this class)
+	Copy(other){
+		// Checks if the other object has the fields for comparison
+		if(other.hasOwnProperty("author") && other.hasOwnProperty("date") && other.hasOwnProperty("link") && other.hasOwnProperty("title") && other.hasOwnProperty("text") && other.hasOwnProperty("authorImage") && other.hasOwnProperty("imageLinks")){
+			this.author = other.author;
+			this.authorImage = other.authorImage;
+			this.date = other.date;
+			this.link = other.link;
+			this.title = other.title;
+			this.text = other.text;
+			this.imageLinks = other.imageLinks;
+		}else{
+			console.log("Object passed for copying does not have the necessary properties");
+		}
+	}
+
+	// Compares the fields of two objects
+	Equals(other){
+		// Checks if the other object has the fields for comparison
+		if(other.hasOwnProperty("author") && other.hasOwnProperty("date") && other.hasOwnProperty("link") && other.hasOwnProperty("title") && other.hasOwnProperty("text") && other.hasOwnProperty("authorImage") && other.hasOwnProperty("imageLinks")){
+			if((this.author != other.author))
+				return false;
+			if((this.date != other.date))
+				return false;
+			if((this.link != other.link))
+				return false;
+			if((this.title != other.title))
+				return false;
+			// Commenting this last comparison because it seems unnecessary
+			// (Also because I'd have to recreate the json file after changing the text parsing method)
+			/*
+			if((this.text != other.text))
+				return false;
+			*/
+		}else{
+			console.log("Object passed for comparison does not have the necessary properties");
+		}
+		return true;
 	}
 };
--- getCode.js
+++ getCode.js
@@ -1,23 +1,67 @@
 "use strict";
+// Modules
 var http = require("http");
+var fs = require("fs");
 var DesignNote = require ("./designNotes.js");
+// Variables
 var pageUrl = "http://www.dragonfable.com/gamedesignnotes/"
+var lastCheckLocation = "./lastCheck.json"
 var codeString = "";
 
 var response = http.get(pageUrl, function(response){
+	// The page html code will be received as a text string
 	response.setEncoding("utf8");
 	response.on("error", console.error);
+	// The data is stored in a global variable
 	response.on("data", function(data){
 		codeString += data;
 	});
+	// When the entire page code has been received, the info will be analyzed
 	response.on("end", function(){
+		// split code between DN posts
 		var stringArray = codeString.split("<td align=\"center\" valign=\"top\">");
 		var currentPosts = [];
-		for(var i = 1; i < (stringArray.length - 1); i++){
+		// Ignores the first code block, as it contains no relevant info for this
+		for(var i = 1; i < stringArray.length; i++){
+			// Create an array with all the posts contained in the html code
 			currentPosts[i-1] = new DesignNote(stringArray[i]);
 		}
-		for(var i = 0; i < currentPosts.length; i++){
-			console.log("\n------------------------------------------\n" + currentPosts[i].text + "\n------------------------------------------\n");
+		
+		// Loads the last saved design notes list from json file
+		try{
+			var fileArray = JSON.parse(fs.readFileSync(lastCheckLocation));
+		}catch(error){
+			// If the file does not exist, creates a file with an empty array and opens it
+			if(error.code === "ENOENT"){
+				console.log(lastCheckLocation + " was not found, creating the file with an empty array...");
+				var emptyArray = [];
+				fs.writeFileSync(lastCheckLocation, JSON.stringify(emptyArray));
+				fileArray = JSON.parse(fs.readFileSync(lastCheckLocation));
+			}else throw error;
 		}
+		// Creates an array of DesignNote class elements to store the file contents (should be unnecessary honestly)
+		var lastCheckPosts = [];
+		for(var i = 0; i < fileArray.length; i++){
+			lastCheckPosts[i] = new DesignNote("");
+			lastCheckPosts[i].Copy(fileArray[i]);
+		}
+
+		// Compares with current posts, and prints all new posts, ignoring duplicates
+		for(var i = 0; i < currentPosts.length; i++){
+			var newPost = true;
+			for(var j = 0; j < lastCheckPosts.length && newPost; j++){
+				if(lastCheckPosts[j].Equals(currentPosts[i]))
+					newPost = false;
+			}
+			if(newPost){
+				//TO DO
+				//Format a discord embed and send it somewhere as a string
+				//DEBUG: Write a notification when a new post is detected
+				console.log("\n===========================================================================================\n\tNew design notes by " + currentPosts[i].author + " detected!\tDate: "+ currentPosts[i].date + "\n" + "\tText: " + currentPosts[i].text);
+			}
+		}
+		
+		// Saves current list to file
+		fs.writeFileSync(lastCheckLocation, JSON.stringify(currentPosts));
 	});
 }).on("error", console.error);
